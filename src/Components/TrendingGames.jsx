7026ef649f218fcfd3c3b4c8fb94b5f829d34fd8
coderbekhub <coderbek@mail.ru>
2024-01-08 09:07:42 +0500

    ...

--- src/Components/TrendingGames.jsx
+++ src/Components/TrendingGames.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function TrendingGames() {
   return (
-    <div>TrendingGames</div>
+    <section className='TrendingGameSection'>
+      <div className="container">
+        <div className='trendingContent'>
+          <h2>Currently Trending Games</h2>
+          <a href="#">SEE ALL</a>
+        </div>
+      </div>
+    </section>
   )
 }
